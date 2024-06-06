c8453b75143bba346b1e0155d0de1da7ed0632b1
PandaTrumpet <dsavontrumpet@gmail.com>
2024-06-06 17:56:28 +0300

    update

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,6 @@
-export const PATH_DB =
+import path from 'node:path';
+import fs from 'node:fs';
+const main = path.join(process.cwd());
+export const PATH_DB = path.join(main, 'src/db', 'db.json');
+const read = fs.readFileSync(PATH_DB);
+console.log(read.toString());
