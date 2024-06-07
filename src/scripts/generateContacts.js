74af95e726766a464b15c7784e872cdca5bb50f0
PandaTrumpet <dsavontrumpet@gmail.com>
2024-06-07 23:52:05 +0300

    update

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,13 +1,5 @@
 import { PATH_DB } from '../constants/contacts.js';
-import fs from 'fs/promises';
-import { createFakeContact } from '../utils/createFakeContact.js';
-const generateContacts = async () => {
-  try {
-    const data = createFakeContact();
-    fs.appendFile(PATH_DB, data, 'utf-8');
-  } catch (error) {
-    console.log(error);
-  }
-};
 
-await generateContacts();
+const generateContacts = async (number) => {};
+
+await generateContacts(5);
