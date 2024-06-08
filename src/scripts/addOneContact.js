2bba00f7119b3a410b202ebe0b935a795a6d391a
PandaTrumpet <dsavontrumpet@gmail.com>
2024-06-08 13:56:04 +0300

    update

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,5 +1,20 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import fs from 'fs/promises';
+export const addOneContact = async () => {
+  try {
+    const mainFile = await fs.readFile(PATH_DB, 'utf-8');
+    const parseFile = JSON.parse(mainFile);
+    const newContact = createFakeContact();
 
-export const addOneContact = async () => {};
+    const updatedData = [...parseFile, newContact];
+
+    const updateJSON = JSON.stringify(updatedData, null, 2);
+    await fs.writeFile(PATH_DB, updateJSON, 'utf-8');
+    console.log(updateJSON);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 await addOneContact();
